d8b48a0706064d1ec951549baf8dfc46201ba617
IgorVasic <igor25vasic@gmail.com>
2020-09-05 13:37:43 +0200

    rekonstrukcija

--- app/app.py
+++
@@ -1,9 +0,0 @@
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import auth
-
-cred = credentials.Certificate("credentials/autentificationfirebase-2a9f4-firebase-adminsdk-5axd2-196b6c35a9 (1).json")
-firebase_admin.initialize_app(cred)
-
-
-auth.create_user(email="igor25vasic@gmail.com", password="Pa$$word")
