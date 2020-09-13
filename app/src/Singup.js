19d09d4a3a2b9f3275f69d45f2a5adfa307cbd47
danielvasic <dvasic1@gmail.com>
2020-09-13 20:29:12 +0200

    Promjene oko učitavanja CSS-a

--- app/src/Singup.js
+++ app/src/Singup.js
@@ -6,6 +6,7 @@ import TextField from '@material-ui/core/TextField';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
+import PropTypes from 'prop-types';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
@@ -15,87 +16,85 @@ import Container from '@material-ui/core/Container';
 import { withStyles } from '@material-ui/core/styles';
 
 
-  
-
-const useStyles = makeStyles((theme) => ({
-    paper: {
-      marginTop: theme.spacing(8),
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-    },
-    avatar: {
-      margin: theme.spacing(1),
-      backgroundColor: theme.palette.secondary.main,
-    },
-    form: {
-      width: '100%', // Fix IE 11 issue.
-      marginTop: theme.spacing(3),
-    },
-    submit: {
-      margin: theme.spacing(3, 0, 2),
-    },
-  }));//sav css
 
 
-class Singup extends React.Component{
-    constructor(props) {
-        super(props);
-        this.state= { name: '',srname:'',reg:'', psw:'',cnfpsw:'', message:'','code':""};
-        //code varijabla za status 200 300 400 500
-        //message varijabla 
-        //varijabla state salje vrijednosti korisnika putem post metode
-        
-    }
-    handleSubmit = (event) => {
-        let self = this;
-        fetch('http://localhost:5000/api/signin', {
-        method: 'POST',
-        // We convert the React state to JSON and send it as the POST body
-        body: JSON.stringify(this.state)
-      }).then(function(response) {//response odgovor sa servera najbitniji
-        var json = response.json();
-            self.setState({ "code": response.status });
-            json.then(function (data) {
-                    self.setState({ "message": data.message });
-                });
-                return json;
-        
+const useStyles = theme => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+});//sav css
+
+
+class Singup extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: '', srname: '', reg: '', psw: '', cnfpsw: '', message: '', code: "" };
+    //code varijabla za status 200 300 400 500
+    //message varijabla 
+    //varijabla state salje vrijednosti korisnika putem post metode
+
+  }
+  handleSubmit = (event) => {
+    let self = this;
+    fetch('http://localhost:5000/api/signin', {
+      method: 'POST',
+      // We convert the React state to JSON and send it as the POST body
+      body: JSON.stringify(this.state)
+    }).then(function (response) {//response odgovor sa servera najbitniji
+      var json = response.json();
+      self.setState({ "code": response.status });
+      json.then(function (data) {
+        self.setState({ "message": data.message });
       });
-        event.target.reset();
-        event.preventDefault();
+      return json;
+
+    });
+    event.target.reset();
+    event.preventDefault();
+  }
+
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+
+  render() {
+    const { classes } = this.props;
+
+    function Copyright() {
+      return (
+        <Typography variant="body2" color="textSecondary" align="center">
+          {'Copyright © '}
+          <Link color="inherit" href="https://material-ui.com/">
+            Your Website
+                </Link>{' '}
+          {new Date().getFullYear()}
+          {'.'}
+        </Typography>
+      );
     }
 
-    handleChange = (event) => {
-        this.setState({[event.target.name]: event.target.value});
-      }
-
-      
-    render(){
-        const { classes } = this.props;
-        function Copyright() {
-            return (
-              <Typography variant="body2" color="textSecondary" align="center">
-                {'Copyright © '}
-                <Link color="inherit" href="https://material-ui.com/">
-                  Your Website
-                </Link>{' '}
-                {new Date().getFullYear()}
-                {'.'}
-              </Typography>
-            );
-          }
-           
-        
-        return( 
-        //onchange uzima ono sto stavimo u input i spasava u state varijablu 
-        
-        
-        <Container component="main" maxWidth="xs">
+    return (
+      //onchange uzima ono sto stavimo u input i spasava u state varijablu 
+      <Container component="main" maxWidth="xs">
         <CssBaseline />
         <div className={classes.paper}>
           <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
+            <LockOutlinedIcon />
           </Avatar>
           <Typography align="center" component="h1" variant="h5" >
             Registracija
@@ -104,7 +103,7 @@ class Singup extends React.Component{
             <Grid container spacing={2}>
               <Grid item xs={12} sm={6}>
                 <TextField
-        
+
                   onChange={this.handleChange}
                   autoComplete="fname"
                   name="firstName"
@@ -113,39 +112,39 @@ class Singup extends React.Component{
                   fullWidth
                   id="firstName"
                   label="Ime"
-                  value = {this.props.name}
+                  value={this.props.name}
                   autoFocus
                 />
               </Grid>
               <Grid item xs={12} sm={6}>
                 <TextField
-                
-                onChange={this.handleChange}
+
+                  onChange={this.handleChange}
                   variant="outlined"
                   required
                   fullWidth
                   id="lastName"
                   label="Prezime"
-                  value = {this.props.srname}
+                  value={this.props.srname}
                   autoComplete="lname"
                 />
               </Grid>
               <Grid item xs={12}>
                 <TextField
-            
-                onChange={this.handleChange}
+
+                  onChange={this.handleChange}
                   variant="outlined"
                   required
                   fullWidth
                   id="email"
                   label="Email Adresa"
-                  value = {this.props.email}
+                  value={this.props.email}
                   autoComplete="email"
                 />
-                </Grid>
-                <Grid item xs={12}>
+              </Grid>
+              <Grid item xs={12}>
                 <TextField
-                onChange={this.handleChange}
+                  onChange={this.handleChange}
                   variant="outlined"
                   required
                   fullWidth
@@ -156,11 +155,11 @@ class Singup extends React.Component{
               </Grid>
               <Grid item xs={12}>
                 <TextField
-                onChange={this.handleChange}
+                  onChange={this.handleChange}
                   variant="outlined"
                   required
                   fullWidth
-                  value = {this.props.psw}
+                  value={this.props.psw}
                   label="Lozinka"
                   type="password"
                   id="password"
@@ -172,23 +171,23 @@ class Singup extends React.Component{
                   variant="outlined"
                   required
                   fullWidth
-                  value = {this.props.cnfpsw}
+                  value={this.props.cnfpsw}
                   label="Potvrda lozinke"
                   type="password"
                 />
               </Grid>
             </Grid>
-            <Button mt={5} 
-           
+            <Button mt={5}
+
               type="submit"
               fullWidth
               variant="contained"
               color="primary"
-               >
+            >
               Registriraj se
             </Button>
-            
-            
+
+
             <Grid container justify="flex-end">
               <Grid item>
                 <Link href="#" variant="body2">
@@ -199,11 +198,12 @@ class Singup extends React.Component{
           </form>
         </div>
         <Box mt={5}>
-        <Copyright />
-      </Box>
+          <Copyright />
+        </Box>
       </Container>
-      
-            );
-    };
+
+    );
+  };
 }
-export default withStyles(useStyles)(Singup);
+
+export default withStyles(useStyles, { withTheme: true })(Singup);
